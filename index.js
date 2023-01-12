03fda638922d4aff1c19ffaacb7069cee4e2407f
Cesar Colunga <cesara.colunga@gmail.com>
2023-01-12 11:03:59 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'The Greatest';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
